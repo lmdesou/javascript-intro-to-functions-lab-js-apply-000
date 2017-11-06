e91845ca1ecc6904f3d3d3c18f9793b2f2e2763a
Lalita Desouza <lmdesou@yahoo.com>
2017-11-06 17:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(logWhisper()) {
+function sayHiToGrandma(string) {
   return "I can't hear you!"
 }
