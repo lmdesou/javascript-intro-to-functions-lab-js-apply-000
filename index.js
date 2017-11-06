8a08548c0d40cfa4fad8056d4c5fe2be48610b31
Lalita Desouza <lmdesou@yahoo.com>
2017-11-06 18:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === .toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
   return
